84c2a6b5654e15eb5013deae7a57086f9116a3b6
whabc100 <whabc100@163.com>
2017-02-09 16:32:20 +0800

    django tutorial 1

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from article import views
-urlpatterns = [
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'my_blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home),
-    url(r'^(?P<my_args>\d+)/$', views.detail, name='detail'),
-    url(r'^test/$', views.test),
-]
+    url(r'^$', 'article.views.home', name = 'home'),
+    url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
+)
+ 
+ #url(r'^(?P<id>\d+)/$'
